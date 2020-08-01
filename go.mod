ed03b069e9be998deb275445231b283738f58269
snwolyg <569616226@qq.com>
2020-08-02 00:21:45 +0800

    部署问题，不能使用 supersior 部署，不执行 ffmpeg 直接 nohup 部署

--- go.mod
+++ go.mod
@@ -3,20 +3,10 @@ module github.com/snowlyg/go_darwin
 go 1.14
 
 require (
-	github.com/CloudyKit/jet/v3 v3.0.0 // indirect
 	github.com/Joker/hpp v1.0.0 // indirect
-	github.com/ajg/form v1.5.1 // indirect
-	github.com/auth0/go-jwt-middleware v0.0.0-20190805220309-36081240882b
-	github.com/dgraph-io/badger v1.6.0 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/etcd-io/bbolt v1.3.3 // indirect
-	github.com/fasthttp-contrib/websocket v0.0.0-20160511215533-1f3b11f56072 // indirect
-	github.com/gavv/httpexpect v2.0.0+incompatible // indirect
 	github.com/go-cmd/cmd v1.2.1
 	github.com/go-redis/redis/v7 v7.2.0
-	github.com/google/go-querystring v1.0.0 // indirect
-	github.com/gorilla/mux v1.7.4 // indirect
-	github.com/imkira/go-interpol v1.1.0 // indirect
+	github.com/jander/golog v0.0.0-20150917071935-954a5be801fc
 	github.com/jinzhu/gorm v1.9.15
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/kardianos/service v1.1.0
@@ -24,25 +14,16 @@ require (
 	github.com/kr/pretty v0.1.0
 	github.com/mattn/go-colorable v0.1.7 // indirect
 	github.com/mattn/go-sqlite3 v2.0.1+incompatible // indirect
-	github.com/moul/http2curl v1.0.0 // indirect
 	github.com/onsi/ginkgo v1.12.2 // indirect
 	github.com/orcaman/concurrent-map v0.0.0-20190826125027-8c72a8bb44f6
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pelletier/go-toml v1.8.0 // indirect
 	github.com/rs/cors v1.7.0
 	github.com/satori/go.uuid v1.2.0
-	github.com/sergi/go-diff v1.1.0 // indirect
-	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.5.0
 	github.com/spf13/viper v1.6.3
 	github.com/stretchr/testify v1.6.1
 	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
-	github.com/urfave/negroni v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.15.1 // indirect
-	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
-	github.com/yudai/gojsondiff v1.0.0 // indirect
-	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
 	github.com/yudai/pp v2.0.1+incompatible // indirect
 	github.com/zserge/lorca v0.1.9
 	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c // indirect
--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/go-cmd/cmd"
+	"github.com/jander/golog/logger"
+	"github.com/kardianos/service"
 	"github.com/snowlyg/go_darwin/client"
 	"github.com/snowlyg/go_darwin/configure"
 	"github.com/snowlyg/go_darwin/models"
@@ -17,7 +18,13 @@ import (
 	"github.com/snowlyg/go_darwin/protocol/httpflv"
 	"github.com/snowlyg/go_darwin/protocol/router"
 	"github.com/snowlyg/go_darwin/protocol/rtmp"
+	"github.com/snowlyg/go_darwin/utils"
 )
+
+func init() {
+	rotatingHandler := logger.NewRotatingHandler(utils.LogDir(), "log.log", 4, 4*1024*1024)
+	logger.SetHandlers(logger.Console, rotatingHandler)
+}
 
 var Version = "master"
 
@@ -25,17 +32,17 @@ func startHls() *hls.Server {
 	hlsAddr := configure.Config.GetString("hls_addr")
 	hlsListen, err := net.Listen("tcp", hlsAddr)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(err)
 	}
 
 	hlsServer := hls.NewServer()
 	go func() {
 		defer func() {
 			if r := recover(); r != nil {
-				log.Println("HLS server panic: ", r)
+				logger.Println("HLS server panic: ", r)
 			}
 		}()
-		log.Println("HLS listen On ", hlsAddr)
+		logger.Println("HLS listen On ", hlsAddr)
 		hlsServer.Serve(hlsListen)
 	}()
 	return hlsServer
@@ -48,24 +55,24 @@ func startRtmp(stream *rtmp.RtmpStream, hlsServer *hls.Server) {
 
 	rtmpListen, err := net.Listen("tcp", rtmpAddr)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(err)
 	}
 
 	var rtmpServer *rtmp.Server
 
 	if hlsServer == nil {
 		rtmpServer = rtmp.NewRtmpServer(stream, nil)
-		log.Println("HLS server disable....")
+		logger.Println("HLS server disable....")
 	} else {
 		rtmpServer = rtmp.NewRtmpServer(stream, hlsServer)
-		log.Println("HLS server enable....")
+		logger.Println("HLS server enable....")
 	}
 	defer func() {
 		if r := recover(); r != nil {
-			log.Println("RTMP server panic: ", r)
+			logger.Println("RTMP server panic: ", r)
 		}
 	}()
-	log.Println("RTMP Listen On ", rtmpAddr)
+	logger.Println("RTMP Listen On ", rtmpAddr)
 	rtmpServer.Serve(rtmpListen)
 }
 
@@ -74,17 +81,17 @@ func startHTTPFlv(stream *rtmp.RtmpStream) {
 
 	flvListen, err := net.Listen("tcp", httpflvAddr)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(err)
 	}
 
 	hdlServer := httpflv.NewServer(stream)
 	go func() {
 		defer func() {
 			if r := recover(); r != nil {
-				log.Println("HTTP-FLV server panic: ", r)
+				logger.Println("HTTP-FLV server panic: ", r)
 			}
 		}()
-		log.Println("HTTP-FLV listen On ", httpflvAddr)
+		logger.Println("HTTP-FLV listen On ", httpflvAddr)
 		hdlServer.Serve(flvListen)
 	}()
 }
@@ -94,16 +101,16 @@ func startAPI(stream *rtmp.RtmpStream) {
 	if apiAddr != "" {
 		opListen, err := net.Listen("tcp", apiAddr)
 		if err != nil {
-			log.Println(err)
+			logger.Println(err)
 		}
 		opServer := router.NewServer(stream, rtmpAddr)
 		go func() {
 			defer func() {
 				if r := recover(); r != nil {
-					log.Println("HTTP-API server panic: ", r)
+					logger.Println("HTTP-API server panic: ", r)
 				}
 			}()
-			log.Println("HTTP-API listen On ", apiAddr)
+			logger.Println("HTTP-API listen On ", apiAddr)
 			opServer.Serve(opListen)
 		}()
 	}
@@ -112,7 +119,7 @@ func startAPI(stream *rtmp.RtmpStream) {
 func startGo() {
 	server := client.GetServer()
 	go func() {
-		log.Println("start go")
+		logger.Println("start go")
 		pusher2ffmpegMap := make(map[*client.Pusher]*cmd.Cmd)
 		var pusher *client.Pusher
 		addChnOk := true
@@ -120,17 +127,17 @@ func startGo() {
 		for addChnOk || removeChnOk {
 			select {
 			case pusher, addChnOk = <-server.AddPusherCh:
-				log.Println("AddPusherCh:", pusher)
+				logger.Println("AddPusherCh:", pusher)
 				if addChnOk {
 					args := []string{"-re", "-rtsp_transport", "tcp", "-i", fmt.Sprintf("%s", pusher.Source), "-c", "copy", "-f", "flv", fmt.Sprintf("rtmp://%s:1935/godarwin/%s", "127.0.0.1", pusher.Key)}
 					if strings.Contains(pusher.Source, "rtmp") {
 						args = []string{"-re", "-i", fmt.Sprintf("%s", pusher.Source), "-c", "copy", "-f", "flv", fmt.Sprintf("rtmp://%s:1935/godarwin/%s", "127.0.0.1", pusher.Key)}
 					}
 
-					fmt.Println(args)
+					logger.Println(args)
 
 					cmdOptions := cmd.Options{
-						Buffered:  false,
+						Buffered:  true,
 						Streaming: true,
 					}
 
@@ -145,13 +152,13 @@ func startGo() {
 									envCmd.Stdout = nil
 									continue
 								}
-								log.Println(line)
+								logger.Println(line)
 							case line, open := <-envCmd.Stderr:
 								if !open {
 									envCmd.Stderr = nil
 									continue
 								}
-								fmt.Fprintln(os.Stderr, line)
+								logger.Println(os.Stderr, line)
 							}
 						}
 					}()
@@ -159,7 +166,7 @@ func startGo() {
 					<-envCmd.Start()
 					<-doneChan
 				} else {
-					log.Printf("addPusherChan closed")
+					logger.Printf("addPusherChan closed")
 				}
 			case pusher, removeChnOk = <-server.RemovePusherCh:
 				if removeChnOk {
@@ -167,38 +174,64 @@ func startGo() {
 					if goCmd != nil {
 						err := goCmd.Stop()
 						if err != nil {
-							log.Printf("prepare to SIGTERM to process:%v", err)
+							logger.Printf("prepare to SIGTERM to process:%v", err)
 						}
 						delete(pusher2ffmpegMap, pusher)
 					}
 
-					log.Printf("delete ffmpeg from pull stream from pusher[%v]", pusher)
+					logger.Printf("delete ffmpeg from pull stream from pusher[%v]", pusher)
 				} else {
 					for _, goCmd := range pusher2ffmpegMap {
 						if goCmd != nil {
 							err := goCmd.Stop()
 							if err != nil {
-								log.Printf("prepare to SIGTERM to process:%v", err)
+								logger.Printf("prepare to SIGTERM to process:%v", err)
 							}
 						}
 					}
 					pusher2ffmpegMap = make(map[*client.Pusher]*cmd.Cmd)
-					log.Printf("removePusherChan closed")
+					logger.Printf("removePusherChan closed")
 				}
 			}
 		}
 	}()
 }
 
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// 非阻塞启动。异步执行
+	go p.run()
+	return nil
+}
+func (p *program) run() {
+	// 执行内容
+	err := models.Init()
+	if err != nil {
+		return
+	}
+
+	stream := rtmp.NewRtmpStream()
+	hlsServer := startHls()
+	go startGo()
+	go startHTTPFlv(stream)
+	go startAPI(stream)
+	go startRtmp(stream, hlsServer)
+}
+
+func (p *program) Stop(s service.Service) error {
+	return nil
+}
+
 func main() {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Println("live panic: ", r)
+			logger.Println("live panic: ", r)
 			time.Sleep(1 * time.Second)
 		}
 	}()
 
-	fmt.Println(fmt.Sprintf(` 
+	logger.Println(fmt.Sprintf(` 
 ====================================================
   ____  ___  ____    _    ______        _____ _   _ 
  / ___|/ _ \|  _ \  / \  |  _ \ \      / /_ _| \ | |
@@ -210,24 +243,73 @@ func main() {
 
 version: %s`, Version))
 
-	if len(os.Args) > 1 {
-		if os.Args[1] == "version" {
-			log.Println(Version)
+	svcConfig := &service.Config{
+		Name:        "godarwin",
+		DisplayName: "视频监控管理平台",
+		Description: "视频监控管理平台，支持 RTSP,RTMP,FLV,M3U8",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		logger.Println(err)
+	}
+
+	if len(os.Args) == 2 {
+		if os.Args[1] == "install" {
+			err := s.Install()
+			if err != nil {
+				panic(err)
+			}
+			logger.Println("服务安装成功")
 			return
 		}
-	}
 
-	// 执行内容
-	err := models.Init()
+		if os.Args[1] == "remove" {
+			err := s.Uninstall()
+			if err != nil {
+				panic(err)
+			}
+			logger.Println("服务卸载成功")
+			return
+		}
+
+		if os.Args[1] == "start" {
+			err := s.Start()
+			if err != nil {
+				panic(err)
+			}
+			logger.Println("服务启动成功")
+			return
+		}
+
+		if os.Args[1] == "stop" {
+			err := s.Stop()
+			if err != nil {
+				panic(err)
+			}
+			logger.Println("服务停止成功")
+			return
+		}
+
+		if os.Args[1] == "restart" {
+			err := s.Restart()
+			if err != nil {
+				panic(err)
+			}
+
+			logger.Println("服务重启成功")
+			return
+		}
+
+		if os.Args[1] == "version" {
+			fmt.Println(fmt.Sprintf("版本号：%s", Version))
+			return
+		}
+
+	}
+	err = s.Run()
 	if err != nil {
-		return
+		logger.Println(err)
 	}
-
-	stream := rtmp.NewRtmpStream()
-	hlsServer := startHls()
-	startGo()
-	startHTTPFlv(stream)
-	startAPI(stream)
-	startRtmp(stream, hlsServer)
-
 }
